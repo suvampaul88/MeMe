520665ff5371c6c8423fd3a12d926763258f7ebc
Suvam Paul <paulsuvam@gmail.com>
2015-08-22 18:50:16 -0400

    Initial Commit

---
+++ MeMe2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeMe2
+//
+//  Created by Suvam Paul on 8/22/15.
+//  Copyright (c) 2015 Suvam Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
